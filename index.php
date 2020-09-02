89c99e2eb65e698c4d16ac1d556638905148c3b0
Francis <fkahindi@gmail.com>
2020-09-02 16:40:24 +0300

    Structure of index page

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lan="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="author" content="Francis Kahindi">
+        <title>The Beauty of Landscaping</title>
+        <link rel="stylesheet" href="styles/main.css">
+        <style >
+        </style> 
+    </head>
+    <body>
+    <section class="grid-wrapper">
+        <header>
+            <p>Header here</p>
+        </header>
+        
+            <main class="">            
+                <p>main here</p>
+            </main>
+            <aside class="">
+                <p>Aside here</p>
+            </aside>
+       
+        <footer>
+            <p>Footer here</p>
+        </footer>
+    </section>
+    </body>
+</html>
