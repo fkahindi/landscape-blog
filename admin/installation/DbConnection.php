7575698acb84bb28e7b0b180b999d26d4981fb8c
Francis <fkahindi@gmail.com>
2020-10-10 18:26:00 +0300

    .gitignore is now taking effect

--- admin/installation/DbConnection.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/DbCredentials.php';
-
-//Create connection using mysqli object
-$conn = new mysqli(DB_SERVER, DB_USERNAME,DB_PASSWORD,DB_NAME);
-//Check connection
-if($conn->connect_error){
-	die('Could not connect: ' .$conn->connect_error);
-}
--- admin/installation/EmailCredentials.php
+++
@@ -1,5 +0,0 @@
-<?php
-	//Email link sending credentials
-	define ('EMAIL','fkahindi@gmail.com');
-	define ('PASS','ragratbfawzkinzv');		
-?>
